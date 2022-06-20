5e32c7566a4d50dfb03e891ece1491404fe1da34
gealit <gealit0369@yandex.ru>
2022-06-20 22:07:19 +0300

    added login, logout, register functionality. Based on django User model. created templates. next step will be to add tasks, and restrict them from other users.

---
+++ todolist/urls.py
@@ -0,0 +1,11 @@
+from django.contrib.auth.views import LogoutView
+from django.urls import path
+
+from todolist.views import HomeView, LoginPage, RegisterPage
+
+urlpatterns = [
+    path('', HomeView.as_view(), name='home'),
+    path('login', LoginPage.as_view(), name='login'),
+    path('logout', LogoutView.as_view(next_page='home'), name='logout'),
+    path('register', RegisterPage.as_view(), name='register'),
+]
--- todolist/views.py
+++ todolist/views.py
@@ -1,3 +1,35 @@
-from django.shortcuts import render
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django.contrib.auth.views import LoginView
+from django.shortcuts import redirect
+from django.views.generic import ListView, FormView
 
-# Create your views here.
+
+class HomeView(ListView):
+    model = User
+    template_name = 'todolist/home.html'
+
+
+class LoginPage(LoginView):
+    template_name = 'todolist/login.html'
+    redirect_authenticated_user = True
+    next_page = 'home'
+
+
+class RegisterPage(FormView):
+    template_name = 'todolist/register.html'
+    form_class = UserCreationForm
+    success_url = '/'
+
+    def form_valid(self, form):
+        user = form.save()
+        if user is not None:
+            login(self.request, user)
+        return super(RegisterPage, self).form_valid(form)
+
+    def get(self, *args, **kwargs):
+        if self.request.user.is_authenticated:
+            return redirect('home')
+        return super(RegisterPage, self).get(*args, **kwargs)
+
